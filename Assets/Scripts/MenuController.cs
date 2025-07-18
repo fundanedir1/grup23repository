30cbd5c30f98b96dd8ee01d74dd4710e03bacd6d
Funda Ayas <funda.ayas2@gmail.com>
2025-07-18 15:48:52 +0300

    menü geliştirme

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro; 
+
+
+public class MenuController : MonoBehaviour
+{
+    public Button playButton;          
+    public TMP_Text playButtonText;        
+
+    private const string HasPlayedKey = "HasPlayedBefore";  // // PlayerPrefs key
+
+    void Start()
+    {
+        if (PlayerPrefs.GetInt(HasPlayedKey, 0) == 1)
+        {
+            playButtonText.text = "Continue";  
+        }
+        else
+        {
+            playButtonText.text = "Play";      
+        }
+    }
+
+    public void PlayGame()
+    {
+        Debug.Log("Play butonuna basildi.");
+
+      
+
+        PlayerPrefs.SetInt(HasPlayedKey, 1);   
+        PlayerPrefs.Save();
+
+        SceneManager.LoadScene("GameScene");   
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void OpenSettings()
+    {
+        SceneManager.LoadScene("SettingsScene"); 
+    }
+
+    public void OpenCredits()
+    {
+        SceneManager.LoadScene("CreditsScene"); 
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Oyun kapatiliyor...");
+        Application.Quit();
+    }
+}
