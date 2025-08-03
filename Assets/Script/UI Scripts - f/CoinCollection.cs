76a3c1d65eb5f5118875425d6db7e3ede252c289
Funda Ayas <funda.ayas2@gmail.com>
2025-08-03 12:59:20 +0300

    audio added to coincollecton

--- Assets/Script/UI Scripts - f/CoinCollection.cs
+++ Assets/Script/UI Scripts - f/CoinCollection.cs
@@ -4,9 +4,14 @@ using TMPro;
 public class CoinCollection : MonoBehaviour
 {
     public TextMeshProUGUI coinText;
+    public AudioSource audioSource;          // Ses oynatici
+    public AudioClip increaseClip;           // Coin arttiginda calacak ses
+    public AudioClip decreaseClip;           // Coin azaldiginda calacak ses
 
+    private int lastMoney;                   // Onceki para degeri
     private void OnEnable()
     {
+        lastMoney = MoneyManager.Money; // Baslangicta mevcut parayi al
         MoneyManager.OnMoneyChanged += UpdateCoinDisplay;
         UpdateCoinDisplay(MoneyManager.Money);
     }
@@ -18,6 +23,20 @@ public class CoinCollection : MonoBehaviour
 
     private void UpdateCoinDisplay(int currentMoney)
     {
+        // Para artisi veya azalisina göre ses cal
+        if (currentMoney > lastMoney && increaseClip != null)
+        {
+            audioSource.PlayOneShot(increaseClip);
+        }
+        else if (currentMoney < lastMoney && decreaseClip != null)
+        {
+            audioSource.PlayOneShot(decreaseClip);
+        }
         coinText.text = currentMoney.ToString();
+        
+        Debug.Log($"Coin Display Updated: {currentMoney}");
+        
+        // Son degeri guncelle
+        lastMoney = currentMoney;
     }
 }
